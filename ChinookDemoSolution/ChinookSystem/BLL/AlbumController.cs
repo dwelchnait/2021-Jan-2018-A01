fe371e67471dd81b40422b6f62f96919eb129cfb
Don Welch <dwelch@nait.ca>
2021-01-14 15:36:07 -0700

    Create ArtistController with DDL query, Add filter search query to AlbumController, VM class SelectionList

--- ChinookDemoSolution/ChinookSystem/BLL/AlbumController.cs
+++ ChinookDemoSolution/ChinookSystem/BLL/AlbumController.cs
@@ -35,5 +35,24 @@ namespace ChinookSystem.BLL
                 return results.ToList();
             }
         }
+
+        [DataObjectMethod(DataObjectMethodType.Select, false)]
+        public List<ArtistAlbums> Albums_GetAlbumsForArtist(int artistid)
+        {
+            using (var context = new ChinookSystemContext())
+            {
+                //Linq to Entity
+
+                IEnumerable<ArtistAlbums> results = from x in context.Albums
+                                                    where x.ArtistId == artistid
+                                                    select new ArtistAlbums
+                                                    {
+                                                        Title = x.Title,
+                                                        ReleaseYear = x.ReleaseYear,
+                                                        ArtistName = x.Artist.Name
+                                                    };
+                return results.ToList();
+            }
+        }
     }
 }
---
+++ ChinookDemoSolution/WebApp/SamplePages/SearchByDDL.aspx
@@ -0,0 +1,5 @@
+﻿<%@ Page Title="Filter Search Demo" Language="C#" MasterPageFile="~/Site.Master" 
+    AutoEventWireup="true" CodeBehind="SearchByDDL.aspx.cs" 
+    Inherits="WebApp.SamplePages.SearchByDDL" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+</asp:Content>
