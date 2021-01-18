2ce532d2e7324e8fe87135b8fcd35cdab516116d
Don Welch <dwelch@nait.ca>
2021-01-18 14:43:34 -0700

    implementation of MessageUsercontrol

--- ChinookDemoSolution/WebApp/SamplePages/SearchByDDL.aspx
+++ ChinookDemoSolution/WebApp/SamplePages/SearchByDDL.aspx
@@ -1,6 +1,10 @@
 ﻿<%@ Page Title="Filter Search Demo" Language="C#" MasterPageFile="~/Site.Master" 
     AutoEventWireup="true" CodeBehind="SearchByDDL.aspx.cs" 
     Inherits="WebApp.SamplePages.SearchByDDL" %>
+
+<%@ Register Src="~/UserControls/MessageUserControl.ascx" TagPrefix="uc1" 
+    TagName="MessageUserControl" %>
+
 <asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
     <h1> Search Albums by Artist</h1>
     <div class="row">
@@ -17,7 +21,7 @@
     <br /><br />
     <div class="row">
         <div class="offset-3">
-            <asp:Label ID="MessageLabel" runat="server" ></asp:Label>
+            <uc1:MessageUserControl runat="server" ID="MessageUserControl" />
          </div>
     </div>
     <br /><br />
@@ -61,6 +65,7 @@
             <asp:ObjectDataSource ID="ArtistNameListODS" runat="server" 
                 OldValuesParameterFormatString="original_{0}" 
                 SelectMethod="Artists_DDLList" 
+                 OnSelected="SelectCheckForException"
                 TypeName="ChinookSystem.BLL.ArtistController">
             </asp:ObjectDataSource>
          </div>
--- ChinookDemoSolution/WebApp/SamplePages/SearchByDDL.aspx.cs
+++ ChinookDemoSolution/WebApp/SamplePages/SearchByDDL.aspx.cs
@@ -26,39 +26,61 @@ namespace WebApp.SamplePages
 
         protected void LoadArtistList()
         {
-            ArtistController sysmgr = new ArtistController();
-            List<SelectionList> info = sysmgr.Artists_DDLList();
+            //  user friendly error handling (aka try/catch)
 
-            //let's assume the data collection needs to be sorted
-            info.Sort((x,y) => x.DisplayField.CompareTo(y.DisplayField));
+            //use the usercontrol MessageUserControl to manage the
+            //    error handling for the web page when control
+            //    leaves the web page and goes to the class library
 
-            //setup the ddl
-            ArtistList.DataSource = info;
-            //ArtistList.DataTextField = "DisplayField";
-            ArtistList.DataTextField = nameof(SelectionList.DisplayField);
-            ArtistList.DataValueField = nameof(SelectionList.ValueField);
-            ArtistList.DataBind();
+            MessageUserControl.TryRun(() => {
+                //what goes inside the coding block?
+                //your code that would normally be inside the try portion of a try/catch
+                ArtistController sysmgr = new ArtistController();
+                List<SelectionList> info = sysmgr.Artists_DDLList();
 
-            //setup of a prompt line
-            ArtistList.Items.Insert(0, new ListItem("select ...", "0"));
+                //let's assume the data collection needs to be sorted
+                info.Sort((x, y) => x.DisplayField.CompareTo(y.DisplayField));
+
+                //setup the ddl
+                ArtistList.DataSource = info;
+                //ArtistList.DataTextField = "DisplayField";
+                ArtistList.DataTextField = nameof(SelectionList.DisplayField);
+                ArtistList.DataValueField = nameof(SelectionList.ValueField);
+                ArtistList.DataBind();
+
+                //setup of a prompt line
+                ArtistList.Items.Insert(0, new ListItem("select ...", "0"));
+
+            },"Success title message","the succes title and body message are optional");
+           
         }
 
+        #region Error Handling Methods for ODS
+        protected void SelectCheckForException(object sender,
+                ObjectDataSourceStatusEventArgs e)
+        {
+            MessageUserControl.HandleDataBoundException(e);
+        }
+        #endregion
         protected void SearchAlbums_Click(object sender, EventArgs e)
         {
             if (ArtistList.SelectedIndex == 0)
             {
                 //Am I on the first physical line (prompt line) of the DDL
-                MessageLabel.Text = "Select an artist for the search.";
+                MessageUserControl.ShowInfo("Search Selection Concern", "Select an artist for the search.");
                 ArtistAlbumList.DataSource = null;
                 ArtistAlbumList.DataBind();
             }
             else
             {
-                AlbumController sysmgr = new AlbumController();
-                List<ChinookSystem.ViewModels.ArtistAlbums> info = sysmgr.Albums_GetAlbumsForArtist(
-                    int.Parse(ArtistList.SelectedValue));
-                ArtistAlbumList.DataSource = info;
-                ArtistAlbumList.DataBind();
+                MessageUserControl.TryRun(() => {
+                    AlbumController sysmgr = new AlbumController();
+                    List<ChinookSystem.ViewModels.ArtistAlbums> info = sysmgr.Albums_GetAlbumsForArtist(
+                        int.Parse(ArtistList.SelectedValue));
+                    ArtistAlbumList.DataSource = info;
+                    ArtistAlbumList.DataBind();
+                },"Search Results","The list of albums for the selected artist");
+               
             }
         }
     }
